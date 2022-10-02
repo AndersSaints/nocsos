bd5fd31a4dc00ea579ac8586ef4f6b7bfa34f3e9
Anderson Santos <41461610+AndersSaints@users.noreply.github.com>
2022-10-02 04:52:02 +0100

    Project2

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php 
 
-$server = "localhost";
-$user = "root";
-$pass = "";
-$database = "login_register_pure_coding";
+$server = "eu-cdbr-west-03.cleardb.net";
+$user = "b3b2d3e787b7a7";
+$pass = "85be2a1c";
+$database = "heroku_35df51f09fc0ef8";
 
 $conn = mysqli_connect($server, $user, $pass, $database);
 
