69f09c6291e4c53c5422faa865679da898bf81ce
Anderson Santos <41461610+AndersSaints@users.noreply.github.com>
2022-10-06 08:56:41 +0100

    v1.03

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ if (isset($_POST['submit'])) {
 			<div class="input-group">
 				<button name="submit" class="btn">Login</button>
 			</div>
-			<p class="login-register-text">Don't have an account? <a href="register.php">Register Here</a>.</p>
+			<!-- <p class="login-register-text">Don't have an account? <a href="admin.php">Register Here</a>.</p> -->
 		</form>
 	</div>
 </body>
