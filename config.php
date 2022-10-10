000a51f0d0888b6ad4ec0feb0cd579d4388652dc
Anderson Santos <41461610+AndersSaints@users.noreply.github.com>
2022-10-10 23:47:21 +0100

    V1.2p

--- config.php
+++ config.php
@@ -1,12 +1,18 @@
 <?php 
 header("content-type: text/html;charset=utf-8");
 
-$server = "eu-cdbr-west-03.cleardb.net";
-$user = "b8e19376ba7475";
-$pass = "b52364be";
-$database = "heroku_bd843db29bed348";
+$server = "ec2-34-247-72-29.eu-west-1.compute.amazonaws.com";
+$user = "wvaokxafzjzzfg";
+$pass = "af38436f20dfed221790a937adca9fa830c56324f6701c77f4f82daa642955d7";
+$database = "d6vk8d75848bo5";
 
-$conn = mysqli_connect($server, $user, $pass, $database);
+
+// $server = "localhost";
+// $user = "root";
+// $pass = "";
+// $database = "noc_sos";
+
+// $conn = mysqli_connect($server, $user, $pass, $database);
 
 if (!$conn) {
     die("<script>alert('Connection Failed.')</script>");
