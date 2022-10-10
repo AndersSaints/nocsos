e545d012cdc4a2b5fd33c06d67d557511b1427e6
Anderson Santos <41461610+AndersSaints@users.noreply.github.com>
2022-10-10 23:09:02 +0100

    V1.0p

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php 
 header("content-type: text/html;charset=utf-8");
 
-$server = "eu-cdbr-west-03.cleardb.net";
-$user = "b3b2d3e787b7a7";
-$pass = "85be2a1c";
-$database = "heroku_35df51f09fc0ef8";
+$server = "j5zntocs2dn6c3fj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+$user = "qsdtqn8cefen1rju";
+$pass = "hylgp1n1yiwcwoj3";
+$database = "sx2h0n0tr55eul2e";
 
 $conn = mysqli_connect($server, $user, $pass, $database);
 
