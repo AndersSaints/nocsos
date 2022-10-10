6b8d2c6c1fe9c56d0f6db758ffe9861f73718474
Anderson Santos <41461610+AndersSaints@users.noreply.github.com>
2022-10-10 23:58:33 +0100

    V1.3p

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php 
 header("content-type: text/html;charset=utf-8");
 
-$server = "ec2-34-247-72-29.eu-west-1.compute.amazonaws.com";
-$user = "wvaokxafzjzzfg";
-$pass = "af38436f20dfed221790a937adca9fa830c56324f6701c77f4f82daa642955d7";
-$database = "d6vk8d75848bo5";
+$server = "eu-cdbr-west-03.cleardb.net";
+$user = "b3b2d3e787b7a7";
+$pass = "85be2a1c";
+$database = "heroku_35df51f09fc0ef8";
 
 
 // $server = "localhost";
